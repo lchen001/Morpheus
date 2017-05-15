bfd3264eab8416d93771d3d9eff098f8ab22923a
Arun Kumar <arun@Aruns-MacBook-Pro.local>
2017-05-15 16:23:59 -0500

    license author list

--- Examples/RealData_LR_Yelp.r
+++ Examples/RealData_LR_Yelp.r
@@ -1,4 +1,4 @@
-#Copyright 2016 Lingjiao Chen, Version 0.8.
+#Copyright 2016 Lingjiao Chen, Arun Kumar, Jeffrey Naughton, and Jignesh M. Patel, Version 0.8.
 #All rights reserved.
 #
 #Licensed under the Apache License, Version 2.0 (the "License");
